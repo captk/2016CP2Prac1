c2a644a4cbb978db075cb75b274be4418586422d
k <k@debian>
2016-08-04 15:41:13 +0930

    Created empty file, quitegood.cpp

---
+++ 2016CP2Prac1/quitegood.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   quitegood.cpp
+ * Author: k
+ *
+ * Created on 4 August 2016, 3:40 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
